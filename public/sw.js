fb8e827f3378e391288a82e5a7fba3688759c851
Glitch (miguel-nunes) <none>
2022-03-16 16:12:52 +0000

    ⛲️🚔 Checkpoint ./.glitch-assets:21764743/438 ./layout/head.html:21764743/206 ./layout/glitch-footer.html:21764743/1112 ./layout/name.html:21764743/260 ./public/manifest.json:21764743/4 ./public/sw.js:21764743/1788 ./public/styles/style.css:21764743/13322 ./public/styles/themes/custom-theme.css:21764743/3239 ./public/styles/themes/gallery.css:21764743/5476 ./public/styles/themes/menu.css:21764743/2550 ./settings.json:21764743/49 ./public/styles/themes/glitch.css:21764743/14

--- public/sw.js
+++ public/sw.js
@@ -5,27 +5,27 @@
  */
 
 // Specify what we want added to the cache for offline use
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     // Give the cache a name
-    caches.open("glitch-in-bio-pwa").then(cache => {
+    caches.open("glitch-in-bio-pwa").then((cache) => {
       // Cache the homepage and stylesheets - add any assets you want to cache!
       return cache.addAll([
-        "/", 
+        "/",
         "/styles/style.css",
         "/styles/themes/glitch.css",
         "/styles/themes/gallery.css",
         "/styles/themes/menu.css",
-        "/styles/themes/custom-theme.css"
+        "/styles/themes/custom-theme.css",
       ]);
     })
   );
 });
 
 // Network falling back to cache approach
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
+    fetch(event.request).catch(function () {
       return caches.match(event.request);
     })
   );
